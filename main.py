e61452e29b3ea07c89cba24350c0387e702d2c3b
foxyhyun <86210630+foxyhyun@users.noreply.github.com>
2023-10-20 18:18:49 +0900

    전처리중

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# === 날씨 데이터 통합 === 
+# === 데이터 불러오기 및 통합 === 
 weather2017 = pd.read_csv('data/weather/weather2017.csv', encoding='cp949')
 weather2018 = pd.read_csv('data/weather/weather2018.csv', encoding='cp949')
 weather2019 = pd.read_csv('data/weather/weather2019.csv', encoding='cp949')
@@ -12,9 +12,21 @@ weather2022 = pd.read_csv('data/weather/weather2022.csv', encoding='cp949')
 
 weather = pd.concat([weather2017, weather2018, weather2019, weather2020, weather2021, weather2022])
 #print(weather.shape) ==> (52563, 38)
+nsv = pd.read_csv('data/nsv/jeonnam.csv', encoding='cp949')
+# print(nsv.shape) ==> (191003, 17)
+
 # === 날씨 데이터 전처리 ===
 
-# # 1. NaN 개수 확인하기
+# 1. NaN 개수 확인하기
 # print(weather.isnull().sum()) # ==> 지울 것들 : 기온 QC플래그, 강수량 (mm), 강수량 QC플래그, 풍속 QC플래그, 풍향 QC플래그, 습도 QC플래그, 현지기압 QC플래그, 해면기압 QC플래그, 일조 (hr), 일조 QC플래그, 일사(MJ/m2), 일사 QC플래그, 적설 (cm), 3시간신적설 (cm), 운형(운형약어), 최저운고 (100m ), 지면상태(지면상태코드), 현상번호(국내식), 지면온도 QC플래그
-weather = weather.drop(['기온 QC플래그','강수량(mm)','강수량 QC플래그','풍속 QC플래그','풍향 QC플래그','습도 QC플래그','현지기압 QC플래그','해면기압 QC플래그','일조(hr)','일조 QC플래그','일사(MJ/m2)','일사 QC플래그','적설(cm)','3시간신적설(cm)','운형(운형약어)','최저운고(100m )','지면상태(지면상태코드)','현상번호(국내식)','지면온도 QC플래그'], axis=1)
-print(weather.isnull().sum())
+weather = weather.drop(['기온 QC플래그','강수량(mm)','전운량(10분위)','중하층운량(10분위)','강수량 QC플래그','풍속 QC플래그','풍향 QC플래그','습도 QC플래그','현지기압 QC플래그','해면기압 QC플래그','일조(hr)','일조 QC플래그','일사(MJ/m2)','일사 QC플래그','적설(cm)','3시간신적설(cm)','운형(운형약어)','최저운고(100m )','지면상태(지면상태코드)','현상번호(국내식)','지면온도 QC플래그'], axis=1)
+#print(weather.isnull().sum())
+
+# 2. 결측값 채우기(NaN => 이전값 대치)
+weather = weather.fillna(method='ffill')
+#print(weather.isnull().sum())
+
+# 3. 불필요한 변수 제거
+weather = weather.drop(['지점','지점명'],axis=1)
+#nsv = nsv.drop(['id','device_id','data_type','CDMA_tel','ip_addr','state','DoseRate_warn','DoseRate_alert'], axis=1)
+# 4. 
