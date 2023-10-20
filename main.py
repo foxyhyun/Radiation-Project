f3c2766586583cb851758e1a319ef6521a9dd272
foxyhyun <86210630+foxyhyun@users.noreply.github.com>
2023-10-20 17:54:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 날씨 데이터 통합
+# === 날씨 데이터 통합 === 
 weather2017 = pd.read_csv('data/weather/weather2017.csv', encoding='cp949')
 weather2018 = pd.read_csv('data/weather/weather2018.csv', encoding='cp949')
 weather2019 = pd.read_csv('data/weather/weather2019.csv', encoding='cp949')
@@ -11,6 +11,10 @@ weather2021 = pd.read_csv('data/weather/weather2021.csv', encoding='cp949')
 weather2022 = pd.read_csv('data/weather/weather2022.csv', encoding='cp949')
 
 weather = pd.concat([weather2017, weather2018, weather2019, weather2020, weather2021, weather2022])
-#print(weather.columns)
+#print(weather.shape) ==> (52563, 38)
+# === 날씨 데이터 전처리 ===
 
-# 날씨 데이터 전처리
+# # 1. NaN 개수 확인하기
+# print(weather.isnull().sum()) # ==> 지울 것들 : 기온 QC플래그, 강수량 (mm), 강수량 QC플래그, 풍속 QC플래그, 풍향 QC플래그, 습도 QC플래그, 현지기압 QC플래그, 해면기압 QC플래그, 일조 (hr), 일조 QC플래그, 일사(MJ/m2), 일사 QC플래그, 적설 (cm), 3시간신적설 (cm), 운형(운형약어), 최저운고 (100m ), 지면상태(지면상태코드), 현상번호(국내식), 지면온도 QC플래그
+weather = weather.drop(['기온 QC플래그','강수량(mm)','강수량 QC플래그','풍속 QC플래그','풍향 QC플래그','습도 QC플래그','현지기압 QC플래그','해면기압 QC플래그','일조(hr)','일조 QC플래그','일사(MJ/m2)','일사 QC플래그','적설(cm)','3시간신적설(cm)','운형(운형약어)','최저운고(100m )','지면상태(지면상태코드)','현상번호(국내식)','지면온도 QC플래그'], axis=1)
+print(weather.isnull().sum())
